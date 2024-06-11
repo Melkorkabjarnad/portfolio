aeadebb1f91a2c6ceab8877fb04d877fe17578cb
Melkorka <mebj0001@stud.kea.dk>
2024-06-12 00:55:24 +0200

    deleted some layouts

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.getElementById('burger-menu');
-    const menu = document.getElementById('menu');
-
-    burgerMenu.addEventListener('click', () => {
-        menu.classList.toggle('nav-active');
-    });
-});
